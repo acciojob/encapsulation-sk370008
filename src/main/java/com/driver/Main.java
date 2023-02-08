1d2120b0f2b2d76a27d2668db65d70d70d3685d4
Shoaib Akhtar <sk370008@gmail.com>
2023-02-08 11:05:36 +0530

    Solution Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw1 = new RWOnly();
+//        rw1.name = "Shoaib";
+//       this error shows when trying to access private member 'name' has private access in 'com.driver.RWOnly'
+        rw1.setter("Shoaib");
+        rw1.getter();
+//        System.out.println(rw1.getter());
+    }
 }
